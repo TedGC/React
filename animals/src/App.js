94e4ef67202ea6303b8a1af13454427f05ae68ec
TedGC <90265172+TedGC@users.noreply.github.com>
2025-03-13 21:48:45 +0900

    update

---
+++ animals/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+
+    const handleClick = () => {
+        console.log('button was clicked!')
+    }
+    return <div>
+        <button onClick={handleClick}> Add animal</button>
+    </div>
+}
+
+export default App;
---
+++ animals/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/cliientt'
+import App from './App'
+
+const el = document.getElementById('root')
+const root = ReactDOM.createRoot(el);
+
+root.redner(<App />)
