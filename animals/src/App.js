479b9aad2b83a3261f7cafc95e279fcdc342d7a5
TedGC <90265172+TedGC@users.noreply.github.com>
2025-03-14 13:19:25 +0900

    update

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,9 +1,17 @@
 import { useState } from 'react'
 
+function getRandomAniaml() {
+    const animals = ['bird', 'cat', 'cow', 'dog', 'gaotr', 'horse']
+
+    return animals[Math.floior(Math.random() * animals.length)]
+}
+
+
 
 function App() {
 
     const [count, setCount] = useState(0);
+
 
     const handleClick = () => {
         setCount(count + 1);
