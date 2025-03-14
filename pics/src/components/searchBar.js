1be86499fabd71819ac215280590c6fec88be784
TedGC <90265172+TedGC@users.noreply.github.com>
2025-03-14 21:28:02 +0900

    update

--- pics/src/components/searchBar.js
+++ pics/src/components/searchBar.js
@@ -1,12 +1,36 @@
+import { useState } from 'react'
+
 function searchBar({ onSubmit }) {
-    const handleClick = () => {
-        onSubmit('cars')
+    const [term, setTerm] = useState('');
+
+    const handleChange = (event) => {
+        setTerm(event.target.value)
     }
 
 
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault(); //need to watch this lecture #74 again why this is used here
+
+        onSubmit('cars')
+    }
+
     return <div>
-        <button onClick={handleClick}></button>
-    </div>dfasdfsa
+        <form onSubmit={handleFormSubmit}>
+            <input value={term} onChange={handleChange} />
+        </form>
+    </div>
 }
 
-export default searchBar; 
+export default searchBar;
+
+
+/**
+ * 1. user types in input
+ * 2. browser updates the text in the input
+ * 3. browser triggers an event to say the input was updated
+ * 4. we read the value from the input, update state
+ * 5. state update! component rerenders
+ * 6. we provide 'value' prop to input - react changes the input's value 
+ * 
+ */
