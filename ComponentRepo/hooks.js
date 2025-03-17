064097bf96de08adf8658185fdb2e9f6a0790753
TedGC <90265172+TedGC@users.noreply.github.com>
2025-03-17 23:36:53 +0900

    Update hooks.js

--- ComponentRepo/hooks.js
+++ ComponentRepo/hooks.js
@@ -58,3 +58,16 @@ const changeColor = (newColor) => {
 
   setFruit(updatedFruit);
 };
+
+
+
+const [colors, setColors] = useState(['red', 'green']);
+
+const addColorAtIndex = (colorToAdd, index) => {
+  const updatedColors = [
+    ...colors.slice(0, index),
+    colorToAdd,
+    ...colors.slice(index),
+  ];
+  setColors(updatedColors);
+};
