4be776e8306083e1fd72ff870dd5482308b3c0d7
TedGC <90265172+TedGC@users.noreply.github.com>
2025-03-19 23:56:03 +0900

    Update hooks.js

--- ComponentRepo/hooks.js
+++ ComponentRepo/hooks.js
@@ -123,3 +123,34 @@ class Child extends React.Component {
     return <h1>Child Component</h1>;
   }
 }
+
+
+import React, { useState, useRef } from "react";
+
+export default function App() {
+  const prevScrollY = useRef(0);
+  const [goingUp, setGoingUp] = useState(false);
+
+  const onScroll = (e) => {
+    const currentScrollY = e.target.scrollTop;
+    if (prevScrollY.current < currentScrollY && goingUp) {
+      setGoingUp(false);
+    }
+    if (prevScrollY.current > currentScrollY && !goingUp) {
+      setGoingUp(true);
+    }
+    prevScrollY.current = currentScrollY;
+    console.log(goingUp, currentScrollY);
+  };
+
+  return (
+    <div onScroll={onScroll} style={{ height: 300, overflowY: "scroll" }}>
+      {Array(50)
+        .fill("Get the scroll position onScroll in react.")
+        .map((f, i) => {
+          return <p key={i}>{f}</p>;
+        })}
+    </div>
+  );
+}
+
