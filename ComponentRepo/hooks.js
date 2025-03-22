6979c852de73f19a87b406bce03fbcfb6618d9d7
TedGC <90265172+TedGC@users.noreply.github.com>
2025-03-22 22:55:39 +0900

    Update hooks.js

--- ComponentRepo/hooks.js
+++ ComponentRepo/hooks.js
@@ -1,3 +1,23 @@
+export default function App() {
+
+  let inputFields = [];
+  for (let i = 0; i < 5; ++i) {
+    inputFields.push(
+      <div> Field {i}: <input type="text" placeholder="Search" /> </div>
+    );
+  }
+
+  return (
+    <>
+      <h2> Repeat an element n times using JSX </h2>
+      <div>{inputFields}</div>
+    </>
+  );
+}
+
+
+
+
 import React, { useState } from "react";
 import { useCookies } from "react-cookie";
 
