3b7decf6acc8002ff66324fa73d20788bc0871ee
TedGC <90265172+TedGC@users.noreply.github.com>
2025-03-22 22:50:17 +0900

    Update hooks.js

--- ComponentRepo/hooks.js
+++ ComponentRepo/hooks.js
@@ -1,4 +1,27 @@
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const handleClick = (type) => {
+    type === "increment" ? setCounter(counter + 1) : setCounter(counter - 1);
+  };
+
+  return (
+    <div>
+      <h2>Counter: {counter}</h2>
+      <div className="buttons">
+        <button onClick={() => handleClick("increment")}>Increment</button>
+        <button onClick={() => handleClick("decrement")}>Decrement</button>
+      </div>
+    </div>
+  );
+};
+
+
+
+
+
 //creating a menu
+
 
 Input:
 [
