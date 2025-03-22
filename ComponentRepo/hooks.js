e06d9c25ad7d9f4eec52ced7ab28580d90da8095
TedGC <90265172+TedGC@users.noreply.github.com>
2025-03-22 22:53:43 +0900

    Update hooks.js

--- ComponentRepo/hooks.js
+++ ComponentRepo/hooks.js
@@ -1,3 +1,25 @@
+//jestcase test
+
+// App.js
+
+function App() {
+  let [count, setCount] = useState(0);
+
+  const decrement = () => setCount((count -= 1));
+  const increment = () => setCount((count += 1));
+
+  return (
+    <div className="App">
+      <h1>Testing React Hooks</h1>
+      <p>{count}</p>
+      <button onClick={decrement}>-</button>
+
+      <button onClick={increment}>+</button>
+    </div>
+  );
+}
+
+
 //understanding the state
 
 unction App() {
