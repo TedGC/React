fe3b979aee6f96393729a754f949d250d05f1e0f
TedGC <90265172+TedGC@users.noreply.github.com>
2025-03-22 22:53:10 +0900

    Update hooks.js

--- ComponentRepo/hooks.js
+++ ComponentRepo/hooks.js
@@ -1,3 +1,32 @@
+//understanding the state
+
+unction App() {
+
+  const [value, setValue] = React.useState("");
+
+  const handleOnChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  React.useEffect(() => {
+    const timeoutId = setTimeout(
+      () => console.log(`Search function called: "${value}"`),
+      300
+    );
+    return () => clearTimeout(timeoutId);
+  }, [value]);
+
+  return (
+    <>
+      <input onChange={handleOnChange} value={value} placeholder="Search" />
+      <h1>{value}</h1>
+    </>
+  );
+}
+
+
+
+
 //boolean state udpate
 
 function App() {
