60af030eb64d38a54df13cf1346301215fcdedb7
TedGC <90265172+TedGC@users.noreply.github.com>
2025-03-22 22:51:26 +0900

    Update hooks.js

--- ComponentRepo/hooks.js
+++ ComponentRepo/hooks.js
@@ -1,4 +1,34 @@
+//onScroll update 
 
+import React, { useState, useRef } from "react";
+
+export default function App() {
+
+  const prevScrollY = useRef(0);
+  const [goingUp, setGoingUp] = useState(false);
+
+  const onScroll = (e) => {
+    const currentScrollY = e.target.scrollTop;
+    if (prevScrollY.current < currentScrollY && goingUp) {
+      setGoingUp(false);
+    }
+    if (prevScrollY.current > currentScrollY && !goingUp) {
+      setGoingUp(true);
+    }
+    prevScrollY.current = currentScrollY;
+    console.log(goingUp, currentScrollY);
+  };
+
+  return (
+    <div onScroll={onScroll} style={{ height: 300, overflowY: "scroll" }}>
+      {Array(50)
+        .fill("Get the scroll position on scroll in react.")
+        .map((f, i) => {
+          return <p key={i}>{f}</p>;
+        })}
+    </div>
+  );
+}
 
 //how to show a toggle function in React
 
