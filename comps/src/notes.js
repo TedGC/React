5f4dbc6f04ea12d9ea4376c0a12a10f84edab785
TedGC <90265172+TedGC@users.noreply.github.com>
2025-03-24 23:22:55 +0900

    update

---
+++ comps/src/notes.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+/**
+ * 1. list out what a user will do and changes they will see while using your app
+ * 2. categorize each step as 'state' or 'event handler'
+ * 3. gorup common steps. Remove duplicates. Rewrite descriptions
+ * 4. Look at mockup. Remove or simplify parts that aren't changing
+ * 5. Replace remaining elements with text descriptions 
+ * 6. Repeat #4 and #5 with a different variation
+ * 7. Imagine you have to write a function that returns the text of setps #5 and #6. 
+ *      in addition to your component props, what other arugments would you need?
+ * 8. Decide where each event handler + state will be defiend 
+ * 
+ * some other notes to be noted of 
+ * 
+ * 1. event handler hsould be usually be defined in the same component as state
+ * it modifies
+ * 2. it mgiht used in a different component
+ */
