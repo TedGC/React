a4eea9726c8ef1522c4152c1c31b49d810e03338
TedGC <90265172+TedGC@users.noreply.github.com>
2025-03-24 23:24:39 +0900

    update

--- comps/src/components/Dropdown.js
+++ comps/src/components/Dropdown.js
@@ -1,43 +1,51 @@
-
-import { useState } from "react";
+import { useState } from 'react';
+import { GoChevronDown } from 'react-icons/go';
 
 function Dropdown({ options, value, onChange }) {
-    const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
 
-    //refer to #202 
-    const handleClick = () => {
-        setIsOpen(!isOpen)
-    }
+  const handleOptionClick = (option) => {
+    // CLOSE DROPDOWN
+    setIsOpen(false);
+    // WHAT OPTION DID THE USER CLICK ON???
+    onChange(option);
+  };
 
+  const renderedOptions = options.map((option) => {
+    return (
+      <div
+        className="hover:bg-sky-100 rounded cursor-pointer p-1"
+        onClick={() => handleOptionClick(option)}
+        key={option.value}
+      >
+        {option.label}
+      </div>
+    );
+  });
 
-    // refer to #205 
-    const handleOptionClick = (option) => {
-        setIsOpen(false)
-
-        onChange(option)
-    }
-
-    const renderedOptions = options.map((option) => {
-        return <div onClick={() => handleOptionClick(option)} key={option.value}>
-            {option.label}
+  return (
+    <div className="w-48 relative">
+      <div
+        className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full"
+        onClick={handleClick}
+      >
+        {value?.label || 'Select...'}
+        <GoChevronDown className="text-lg" />
+      </div>
+      {isOpen && (
+        <div className="absolute top-full border rounded p-3 shadow bg-white w-full">
+          {renderedOptions}
         </div>
-    })
-
-    let content = 'select... '
-    if (value) {
-        content = value.label
-    }
-
-    return <div>
-        <div onClick={() => handleClick(options)}> {value?.label || 'select...'} </div>
-        {isOpen && <div>{renderedOptions}</div>}
+      )}
     </div>
+  );
 }
 
-
 export default Dropdown;
-
 /**
  * how to think about all the user interactions that would happend for a single 
  * action of clicking the button and opening up the dropdown menu 
