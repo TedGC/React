3e4c0f2b316b69b7d45a10a1db47722e0d7083bd
TedGC <90265172+TedGC@users.noreply.github.com>
2025-03-24 23:51:19 +0900

    udpate

--- ComponentRepo/hooks.js
+++ ComponentRepo/hooks.js
@@ -144,3 +144,54 @@ class Page2 extends React.Component {
 }
 
 export default withRouter(Page2);
+
+
+import React from "react";
+import { Redirect, Switch, Route, withRouter } from "react-router";
+
+import Page1 from "./Page1";
+import Page2 from "./Page2";
+import Page3 from "./Page3";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // Store the previous pathname
+    this.currentPathname = null;
+  }
+
+  componentDidMount() {
+    const { history } = this.props;
+
+    history.listen((newLocation, action) => {
+      if (action === "PUSH") {
+        if (newLocation.pathname !== this.currentPathname) {
+          // Save new location
+          this.currentPathname = newLocation.pathname;
+
+          // Clone location object and push it to history
+          history.push({
+            pathname: newLocation.pathname
+          });
+        }
+      } else {
+        // Send user back if they try to navigate back
+        history.go(1);
+      }
+    });
+  }
+
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" render={() => <Redirect to="/page1" />} />
+        <Route path="/page1" component={Page1} />
+        <Route path="/page2" component={Page2} />
+        <Route path="/page3" component={Page3} />
+      </Switch>
+    );
+  }
+}
+
+export default withRouter(App);
