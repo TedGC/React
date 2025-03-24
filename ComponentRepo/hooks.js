cc244b7d11c9cf2ff37fc714a13015fcdfa7a3d3
TedGC <90265172+TedGC@users.noreply.github.com>
2025-03-24 23:48:27 +0900

    update

--- ComponentRepo/hooks.js
+++ ComponentRepo/hooks.js
@@ -100,3 +100,20 @@ function App() {
     </>
   );
 }
+
+
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import App from "./components/App";
+import "./styles.css";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <BrowserRouter>
+    <Route path="/" component={App} />
+  </BrowserRouter>,
+  rootElement
+);
