faa0dcede06e77779753a04e07a570e7b5871489
TedGC <90265172+TedGC@users.noreply.github.com>
2025-03-25 23:01:37 +0900

    update

--- comps/src/Buttonpage.js
+++ comps/src/Buttonpage.js
@@ -1,48 +1,47 @@
 import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
-import Button from '../compoents/Button';
+import Button from '../components/Button';
 
-function Buttonpage() {
-    const handleClick = () => {
-        console.log('Click!!');
-    };
+function ButtonPage() {
+  const handleClick = () => {};
 
-    return (
-        <div>
-            <div>
-                <Button
-                    secondary
-                    outline
-                    rounded
-                    onClick={handleClick}
-                    className="mb-5">
-                    <GoBell />
-                    Click me!!
-                </Button>
-            </div>
-            <div>
-                <Button danger outline>
-                    <GoCloudDownload />
-                    Buy Now!
-                </Button>
-            </div>
-            <div>
-                <Button warning>
-                    <GoDatabase />
-                    See Deal!
-                </Button>
-            </div>
-            <div>
-                <Button secondary outline>
-                    Hide Ads!
-                </Button>
-            </div>
-            <div>
-                <Button primary rounded>
-                    Something!
-                </Button>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div>
+        <Button
+          secondary
+          outline
+          rounded
+          className="mb-5"
+          onClick={handleClick}
+        >
+          <GoBell />
+          Click me!!
+        </Button>
+      </div>
+      <div>
+        <Button danger outline onMouseEnter={handleClick}>
+          <GoCloudDownload />
+          Buy Now!
+        </Button>
+      </div>
+      <div>
+        <Button warning onMouseLeave={handleClick}>
+          <GoDatabase />
+          See Deal!
+        </Button>
+      </div>
+      <div>
+        <Button secondary outline>
+          Hide Ads!
+        </Button>
+      </div>
+      <div>
+        <Button primary rounded>
+          Something!
+        </Button>
+      </div>
+    </div>
+  );
 }
 
-export default Buttonpage
+export default ButtonPage;
