c49dfd5fadb0990b58af2e3ca24be99f7be662b9
TedGC <90265172+TedGC@users.noreply.github.com>
2025-03-27 09:44:16 +0900

    update

--- ComponentRepo/hooks.js
+++ ComponentRepo/hooks.js
@@ -162,3 +162,34 @@ const App = () => {
     </section>
   );
 }
+
+
+import React, { useState, useRef } from "react";
+
+export default function App() {
+
+  const prevScrollY = useRef(0);
+  const [goingUp, setGoingUp] = useState(false);
+
+  const onScroll = (e) => {
+    const currentScrollY = e.target.scrollTop;
+    if (prevScrollY.current < currentScrollY && goingUp) {
+      setGoingUp(false);
+    }
+    if (prevScrollY.current > currentScrollY && !goingUp) {
+      setGoingUp(true);
+    }
+    prevScrollY.current = currentScrollY;
+    console.log(goingUp, currentScrollY);
+  };
+
+  return (
+    <div onScroll={onScroll} style={{ height: 300, overflowY: "scroll" }}>
+      {Array(50)
+        .fill("Get the scroll position on scroll in react.")
+        .map((f, i) => {
+          return <p key={i}>{f}</p>;
+        })}
+    </div>
+  );
+}
