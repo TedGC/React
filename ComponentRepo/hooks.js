0c245553b6132d0cb3fe1418a1757f556af8f09f
TedGC <90265172+TedGC@users.noreply.github.com>
2025-03-27 09:43:58 +0900

    update

--- ComponentRepo/hooks.js
+++ ComponentRepo/hooks.js
@@ -124,3 +124,41 @@ const App = () => {
     </section>
   );
 }
+
+
+import { Link, BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+const IndexPage = () => {
+  return <h2>Home Page</h2>;
+};
+
+const PropsPage = ({ title }) => {
+  return <h2>{title}</h2>;
+};
+
+const App = () => {
+  return (
+    <section className="App">
+      <Router>
+        <Link to="/">Home</Link> |
+        <Link to="/props-through-component">Props through component</Link> |
+        <Link to="/props-through-render">Props through render</Link> |
+        <Switch>
+          <Route exact path="/" component={IndexPage} />
+          <Route
+            exact
+            path="/props-through-component"
+            component={() => <PropsPage title={`Props through component`} />}
+          />
+          <Route
+            exact
+            path="/props-through-render"
+            render={(props) => (
+              <PropsPage {...props} title={`Props through render`} />
+            )}
+          />
+        </Switch>
+      </Router>
+    </section>
+  );
+}
