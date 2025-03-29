f63b8f519e7cd4c3e26b3acc4ce55f2aa38311db
TedGC <90265172+TedGC@users.noreply.github.com>
2025-03-29 23:20:28 +0900

    update

--- comps/src/page/CounterPage.js
+++ comps/src/page/CounterPage.js
@@ -1,38 +1,115 @@
-import { useState, useEffect } from "react";
+import { useReducer } from "react";
 import Button from '../components/Button'
 import Panel from '../components/Panel'
-function CounterPage({ initialCount }) {
-    const [count, setCount] = useState(initialCount)
-    const [valueToAdd, setValueToAdd] = useState(0)
 
-    useEffect(() => {
-        console.log(count)
-    }, [count])
+
+const INCREMENT_COUNT = 'increment';
+const SET_VALUE_TO_ADD = 'set-value-to-add';
+const DECREMENT_COUNT = 'derement'
+const ADD_VALUE_TO_COUNT = 'add_value_to_count'
+
+//ANOTHER ALTERNATIVE TO THE SCRIPT BELOW 
+
+
+// #299 
+const reducer = (state, action) => {
+    switch (action.type) {
+        case INCREMENT_COUNT:
+            return {
+                ...state,
+                count: state.count + 1
+            }
+        case DECREMENT_COUNT:
+            return {
+                ...state,
+                count: state.count - 1
+            }
+        case SET_VALUE_TO_ADD:
+            return {
+                ...state,
+                valueToAdd: action.payload
+            }
+        case ADD_VALUE_TO_COUNT:
+            return {
+                ...state,
+                count: state.count + state.valueToAdd,
+                valueToAdd: 0
+            }
+        default:
+            return state
+    }
+}
+
+
+
+
+
+
+// const reducer = (state, action) => {
+//     if (action.type === INCREMENT_COUNT) {
+//         return {
+//             ...state,
+//             count: state.count + 1
+//         }
+//     }
+
+//     if (action.type === SET_VALUE_TO_ADD) {
+//         return {
+//             ...state,
+//             valueToAdd: action.payload
+//         }
+//     }
+// }
+
+function CounterPage({ initialCount }) {
+    // const [count, setCount] = useState(initialCount)
+    // const [valueToAdd, setValueToAdd] = useState(0)
+    const [state, dispatch] = useReducer(reducer, {
+        count: initialCount,
+        valueToAdd: 0
+    })
+    console.log(state)
+
 
     const increment = () => {
-        setCount(count + 1)
+        // setCount(count + 1)
+        dispatch({
+            type: INCREMENT_COUNT
+
+        })
     }
 
     const decrement = () => {
-        setCount(count - 1)
+        // setCount(count - 1)
+
+        dispatch({
+            type: DECREMENT_COUNT,
+
+        })
     }
 
     const handleChange = (event) => {
         const value = parseInt(event.target.value) || 0;
 
-        setValueToAdd(value)
+        dispatch({
+            type: SET_VALUE_TO_ADD,
+            payload: value
+        })
     }
-
     const handleSubmit = (event) => {
         event.preventDefault()
 
-        setCount(count + valueToAdd)
-        setValueToAdd(0)
+        // setCount(count + valueToAdd)
+        // setValueToAdd(0)
+
+        dispatch({
+            type: ADD_VALUE_TO_COUNT
+        })
     }
 
 
     return <Panel className="m-3">
-        <h1 className="text-lg">Count is {count}</h1>
+        <h1 className="text-lg">Count is {state.count}</h1>
         <div className="flex flex-row">
             <Button onClick={increment}> Increment</Button>
             <Button onClick={decrement}> Decrement</Button>
@@ -41,7 +118,7 @@ function CounterPage({ initialCount }) {
         <form onSubmit={handleSubmit}>
             <label>Add a lot!</label>
             <input
-                value={valueToAdd || ""}
+                value={state.valueToAdd || ""}
                 type="number"
                 onChange={handleChange}
                 className="p-1 m-3 bg-gray-50 border border-gray-300" />
@@ -63,4 +140,45 @@ export default CounterPage
  * 5. in your hook, return an object that onctains that variables the component needs
  * 6. in the component, call your hook. Destructure the 
  * 
+ */
+
+
+
+
+
+/**
+ * 
+ * 1. the store is an object that will hold all of our state
+ * 2. we usually do not have to interact with it directly. The 'react-redux 
+ * library will handle the store for us 
+ * 3. to dispatch an action
+ * 4. to see what state exists in the store 
+ * 
+ */
+
+
+/**
+ * 
+ * 
+ * changing state using Redux
+ * 
+ * 1. add a reducer to one of your slices that changes state in some particular
+ * way
+ * 2. export the action creator that the slice automatically creates
+ * 3. find the component that you want to dispatch from
+ * 4. import the action creator function and 'useDispatch' from react-redux
+ * 5. call the 'useDispatch' hook to get access to the dispatch function 
+ * 6. when the user does something ,call the action creator to get an action, 
+ * then dispatch it 
+ */
+
+
+/**
+ * accessing state
+ * 
+ * 1. find the component that needs to access some state
+ * 2. import the 'useSelector' hook from 'react-redux'
+ * 3. call the hook, passing in a selector function
+ * 4. use the state! anaytime state changes, the component will automatically
+ * rerender 
  */
