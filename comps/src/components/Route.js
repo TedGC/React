c427b8dbd2b24e3fde4d4e6244fb01d2992c8e00
TedGC <90265172+TedGC@users.noreply.github.com>
2025-03-29 23:25:25 +0900

    update

--- comps/src/components/Route.js
+++ comps/src/components/Route.js
@@ -1,8 +1,9 @@
+
 import useNavigation from '../hooks/use-navigation';
 
-function Route({ path, children }) {
-  const { currentPath } = useNavigation();
 
+function Route({ path, children }) {
+  const { currentPath } = useNavigation()
   if (path === currentPath) {
     return children;
   }
