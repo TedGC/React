79799c554ef83710b2a4610f9b931a2ab27a2e24
TedGC <90265172+TedGC@users.noreply.github.com>
2025-04-01 23:29:59 +0900

    update

--- media/src/store/index.js
+++ media/src/store/index.js
@@ -1,19 +1,31 @@
+
 import { configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query'
 import { usersReducer } from './slices/usersSlice'
+import { albumsApi } from './apis/albumsApi'
 
 export const store = configureStore({
     reducer: {
-        users: usersReducer
+        users: usersReducer,
+        // if it is a diffuclt concept, need to check out #399
+        [albumsApi.reducerPath]: albumsApi.reducer
+    },
+    middleware: (getDefaultMiddleware) => {
+        return getDefaultMiddleware()
+            .concat(albumsApi.middleware)
     }
 });
 
+setupListeners(store.dispatch)
+
 
 export * from './thunks/fetchUsers'
-
-
-
-
-
+export * from './thunks/addUsers'
+export * from './thunks/removeUser'
+export {
+    useFetchAlbumsQuery,
+    useAddAlbumMutation
+} from './apis/albumsApi'
 
 
 
