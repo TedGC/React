6c354b1f276190998ea4e843a81e9c03ba3732bd
TedGC <90265172+TedGC@users.noreply.github.com>
2025-04-02 22:24:06 +0900

    update

---
+++ media/components/PhotosListItem.js
@@ -0,0 +1,23 @@
+import { useRemovePhotoMutation } from "../store"
+import { GoTrashcan } from "react-icons/go"
+
+
+function PhotosListItem({ photo }) {
+
+    const [removePhoto] = useRemovePhotoMutation()
+
+    const handleRemovePhoto = () => {
+        removePhoto(photo)
+
+    }
+    return <div onClick={handleRemovePhoto} className="relative cursor-pointer">
+        <img className="h-20 w-20" src={photo.url} alt="random pic" />
+        <div className="absolute inset-0 flex items-center justify-center
+        hover:bg-gray-200 opacity-0 hover:opacicy-80" >
+            <GoTrashcan className="text-3xl" />
+        </div>
+
+    </div>
+}
+
+export default PhotosListItem
