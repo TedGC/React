1c2883b4b969fb96f5296a29520867519729ca81
TedGC <90265172+TedGC@users.noreply.github.com>
2025-04-02 22:18:43 +0900

    update

--- media/components/AlbumsListItem.js
+++ media/components/AlbumsListItem.js
@@ -1,17 +1,33 @@
-import Button from "./Button";
-import ExpandablePanel from "./ExpandablePanel";
+import { GoTrashcan } from 'react-icons/go';
+import Button from './Button';
+import ExpandablePanel from './ExpandablePanel';
+import { useRemoveAlbumMutation } from '../store';
+import PhotosList from './PhotosList';
 
 function AlbumsListItem({ album }) {
-    const header = <div>{album.title}</div>
 
-    return (
+  const [removeAlbum, results] = useRemoveAlbumMutation();
 
-        <ExpandablePanel key={album.id} header={header} >
-            List of phots
-        </ExpandablePanel>
-    )
+  const handleRemoveAlbum = () => {
+    removeAlbum(album)
+  }
+  const header = (
+    <>
+      <Button
+        className="mr-2"
+        loading={results.isLoading}
+        onClick={handleRemoveAlbum}>
+        <GoTrashcan />
+      </Button>
+      {album.title}
+    </>
+  );
+
+  return (
+    <ExpandablePanel key={album.id} header={header}>
+      <PhotosList album={album} />
+    </ExpandablePanel>
+  );
 }
-
-
 
 export default AlbumsListItem;
