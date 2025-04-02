7bda53b1be6b7c6e03bd050cae051b98181a8a4d
TedGC <90265172+TedGC@users.noreply.github.com>
2025-04-02 22:18:54 +0900

    update

--- media/components/AlbumsList.js
+++ media/components/AlbumsList.js
@@ -1,39 +1,39 @@
-import { useFetchAlbumsQuery, useAddAlbumMutation } from "../store"
-import Skeleton from './skeleton'
-import ExpandablePanel from "./ExpandablePanel"
-import Button from "./Button"
-import AlbumsListItem from "./AlbumsListItem"
-
-
+import { useFetchAlbumsQuery, useAddAlbumMutation } from '../store';
+import Skeleton from './Skeleton';
+import ExpandablePanel from './ExpandablePanel';
+import Button from './Button';
+import AlbumsListItem from './AlbumsListItem';
 
 function AlbumsList({ user }) {
-    const { data, error, isLoading } = useFetchAlbumsQuery(user)
-    const [addAlbum, results] = useAddAlbumMutation()
+  const { data, error, isFetching } = useFetchAlbumsQuery(user);
+  const [addAlbum, results] = useAddAlbumMutation();
 
-    const handleAddAlbum = () => {
-        addAlbum(user);
-    }
+  const handleAddAlbum = () => {
+    addAlbum(user);
+  };
 
-    let content;
-    if (isLoading) {
-        content = <Skeleton className="h-10 w-full" times={3} />
-    } else if (error) {
-        content = <div> Error</div>
-    } else {
-        content = data.map(album => {
-            return <AlbumsListItem key={album.id} album={album} />
-        })
-    }
+  let content;
+  if (isFetching) {
+    content = <Skeleton className="h-10 w-full" times={3} />;
+  } else if (error) {
+    content = <div>Error loading albums.</div>;
+  } else {
+    content = data.map((album) => {
+      return <AlbumsListItem key={album.id} album={album} />;
+    });
+  }
 
-
-    return <div className="m-2 flex flex-row items-center justify-between">
-        <h3 className="text-lg font-bold"> <div>Albums for {user.name}</div> </h3>
-        <Button loading={isLoading} onClick={handleAddAlbum}>
-            + Add Album
+  return (
+    <div>
+      <div className="m-2 flex flex-row items-center justify-between">
+        <h3 className="text-lg font-bold">Albums for {user.name}</h3>
+        <Button loading={results.isLoading} onClick={handleAddAlbum}>
+          + Add Album
         </Button>
-        <div>{content}</div>
+      </div>
+      <div>{content}</div>
     </div>
+  );
 }
 
-
-export default AlbumsList
+export default AlbumsList;
