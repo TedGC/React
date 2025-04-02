addf9f77258bd35e1d027ad55f8a9b6aab4d8999
TedGC <90265172+TedGC@users.noreply.github.com>
2025-04-02 22:21:45 +0900

    update

--- media/src/store/index.js
+++ media/src/store/index.js
@@ -1,31 +1,43 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { usersReducer } from './slices/usersSlice';
+import { albumsApi } from './apis/albumsApi';
+import { photosApi } from './apis/photosApi';
 
-import { configureStore } from '@reduxjs/toolkit'
-import { setupListeners } from '@reduxjs/toolkit/query'
-import { usersReducer } from './slices/usersSlice'
-import { albumsApi } from './apis/albumsApi'
+
 
 export const store = configureStore({
-    reducer: {
-        users: usersReducer,
-        // if it is a diffuclt concept, need to check out #399
-        [albumsApi.reducerPath]: albumsApi.reducer
-    },
-    middleware: (getDefaultMiddleware) => {
-        return getDefaultMiddleware()
-            .concat(albumsApi.middleware)
-    }
+  reducer: {
+    users: usersReducer,
+    [albumsApi.reducerPath]: albumsApi.reducer,
+    [photosApi.reducerPath]: photosApi.reducer,
+
+
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware()
+      .concat(albumsApi.middleware)
+      .concat(photosApi.middleware)
+  },
 });
 
-setupListeners(store.dispatch)
+setupListeners(store.dispatch);
 
-
-export * from './thunks/fetchUsers'
-export * from './thunks/addUsers'
-export * from './thunks/removeUser'
+export * from './thunks/fetchUsers';
+export * from './thunks/addUser';
+export * from './thunks/removeUser';
 export {
-    useFetchAlbumsQuery,
-    useAddAlbumMutation
-} from './apis/albumsApi'
+  useFetchAlbumsQuery,
+  useAddAlbumMutation,
+  useRemoveAlbumMutation,
+} from './apis/albumsApi';
+
+export {
+  useFetchPhotosQuery,
+  useAddPhotoMutation,
+  useRemovePhotoMutation,
+} from './apis/photosApi'
+
 
 
 
