0feefc3eca14170ede106885a3857f25ea937af0
TedGC <90265172+TedGC@users.noreply.github.com>
2025-04-02 22:23:11 +0900

    update

--- media/components/Button.js
+++ media/components/Button.js
@@ -9,12 +9,14 @@ function Button({
   danger,
   outline,
   rounded,
+  loading,
   ...rest
 }) {
   const classes = className(
     rest.className,
-    'flex items-center px-3 py-1.5 border',
+    'flex items-center px-3 py-1.5 border h-8',
     {
+      'opacity-80': loading,
       'border-blue-500 bg-blue-500 text-white': primary,
       'border-gray-900 bg-gray-900 text-white': secondary,
       'border-green-500 bg-green-500 text-white': success,
