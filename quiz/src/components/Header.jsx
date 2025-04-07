7003744faacd4216cf270c2448374ff542044a64
TedGC <90265172+TedGC@users.noreply.github.com>
2025-04-07 22:58:34 +0900

    update

---
+++ quiz/src/components/Header.jsx
@@ -0,0 +1,8 @@
+import logoImg from '../assets/quiz-logo.png'
+
+export default function Header() {
+    return <header>
+        <img src={logoImg} alt='quiz logo baby' />
+        <h1></h1>
+    </header>
+}
