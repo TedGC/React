8d75642ddf45c6462b630151508afb8168a01805
TedGC <90265172+TedGC@users.noreply.github.com>
2025-04-10 23:15:38 +0900

    update

---
+++ Food/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Cart";
+import FoodList
+function App() {
+  return (
+    <Header /> 
+    <FoodList>
+      {children}
+    </FoodList>
+  );
+}
+
+export default App;
