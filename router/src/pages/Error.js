aa92d6fd43390160b79c2aa30086b84d934a8d71
TedGC <90265172+TedGC@users.noreply.github.com>
2025-04-15 22:45:45 +0900

    update

---
+++ router/src/pages/Error.js
@@ -0,0 +1,18 @@
+import MainNavigation from "../components/MainNavigation"
+
+
+function ErrorPage() {
+
+    return (
+        <>
+            <MainNavigation />
+            <main>
+                <h1>An Error Occurred</h1>
+
+            </main>
+        </>
+    )
+}
+
+
+export default ErrorPage
