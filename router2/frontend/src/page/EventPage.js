d35cc9687f6288d5b23f6b605777a52676651ab6
TedGC <90265172+TedGC@users.noreply.github.com>
2025-04-17 22:41:39 +0900

    update

--- router2/frontend/src/page/EventPage.js
+++ router2/frontend/src/page/EventPage.js
@@ -4,6 +4,10 @@ import EventsList from '../components/EventsList';
 function EventsPage() {
 
     const data = useLoaderData()
+
+    // if(data.isError){
+    //     return <p>{data.message}</p>
+    // }
     const events = data.events
 
     return (
@@ -12,18 +16,26 @@ function EventsPage() {
         </>
     );
 }
-
+//loader can bu used within the target component or any compnent that goes within
+// that target component of using the loader
 
 //loader function is not a React component thus not able to use React hooks
 //such as useState, useEffect and whatnot
 export default EventsPage;
 
+//this laoder is still happening on the browser side 
 export async function loader() {
     const response = await fetch('http://localhost:8080/events');
+    //loader function will return async feature automatically as a part of the package
 
     if (!response.ok) {
-        return { isError: true, message: 'could not fetch events' }
-
+        // return { isError: true, message: 'could not fetch events' }
+        throw new Response(JSON.stringify({ message: 'could not fetch evetns' },
+            { status: 500 }))
+        // throw json(
+        //     { message: 'could not fetch events' },
+        //     { status: 500 }
+        // )
     } else {
 
         return response;
@@ -33,7 +45,6 @@ export async function loader() {
         // const res = new Response('', {status: 201})
     }
 }
-
 
 
 
