ab4c7c24a53888de52a146d633463c31fadb4201
TedGC <90265172+TedGC@users.noreply.github.com>
2025-04-17 22:42:07 +0900

    update

--- router2/frontend/src/page/ErrorPage.js
+++ router2/frontend/src/page/ErrorPage.js
@@ -1,16 +1,32 @@
-import MainNavigation from "../components/MainNavigation"
+import PageContent from '../components/PageContent'
+import { useRouteError } from 'react-router-dom'
+
 
 
 function ErrorPage() {
 
-    return (
-        <>
-            <MainNavigation />
-            <main>
-                <h1>An Error Occurred</h1>
+    const error = useRouteError()
 
-            </main>
-        </>
+
+
+    let title = 'An error occurred'
+    let message = 'soemthing went south'
+
+    if (error.status === 500) {
+        message = JSON.parse(error.data).message
+
+        //json.parse removed due to the introduction of json in EventsPage
+    }
+
+    if (error.status === 404) {
+        title = 'not found'
+        message = 'not found either'
+    }
+
+    return (
+        <PageContent title={title}>
+            <p>{message}</p>
+        </PageContent>
     )
 }
 
