567699c3bbb903fc3b781c42d56bf2cd7f8021cf
TedGC <90265172+TedGC@users.noreply.github.com>
2025-04-21 23:31:35 +0900

    update

---
+++ rehash/src/App.jsx
@@ -0,0 +1,15 @@
+import Accordion from "./components/Accordion/accordion.jsx";
+
+function App() {
+  return (
+    <main>
+      <section>
+        <h2>description</h2>
+
+        <Accordion className='accordion' />
+      </section>
+    </main>
+  )
+}
+
+export default App;
